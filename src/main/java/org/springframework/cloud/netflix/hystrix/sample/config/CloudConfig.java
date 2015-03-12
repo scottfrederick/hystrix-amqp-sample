b00240c76f630bfa4cbbde73715eaad882089f93
Scott Frederick <scottyfred@gmail.com>
2015-03-12 17:13:03 -0500

    Rename Connectors base config class.

--- src/main/java/org/springframework/cloud/netflix/hystrix/sample/config/CloudConfig.java
+++ src/main/java/org/springframework/cloud/netflix/hystrix/sample/config/CloudConfig.java
@@ -2,14 +2,14 @@ package org.springframework.cloud.netflix.hystrix.sample.config;
 
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.cloud.netflix.hystrix.amqp.HystrixConnectionFactory;
-import org.springframework.cloud.pivotal.config.java.PivotalCloudConfig;
+import org.springframework.cloud.pivotal.config.java.CloudConnectorsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("cloud")
-public class CloudConfig extends PivotalCloudConfig {
+public class CloudConfig extends CloudConnectorsConfig {
 	@Bean
 	@HystrixConnectionFactory
 	public ConnectionFactory hystrixConnectionFactory() {

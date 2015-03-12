2df4ff3fc07db1fd8b4880b874ef468f6ddef363
Scott Frederick <scottyfred@gmail.com>
2015-03-12 13:36:09 -0500

    Change CloudConfig to explicity create connection factory bean.

--- src/main/java/org/springframework/cloud/netflix/hystrix/sample/config/CloudConfig.java
+++ src/main/java/org/springframework/cloud/netflix/hystrix/sample/config/CloudConfig.java
@@ -1,11 +1,17 @@
 package org.springframework.cloud.netflix.hystrix.sample.config;
 
-import org.springframework.cloud.config.java.ServiceScan;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.cloud.netflix.hystrix.amqp.HystrixConnectionFactory;
+import org.springframework.cloud.pivotal.config.java.PivotalCloudConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@ServiceScan
 @Profile("cloud")
-public class CloudConfig {
+public class CloudConfig extends PivotalCloudConfig {
+	@HystrixConnectionFactory
+	public ConnectionFactory hystrixConnectionFactory() {
+		return connectionFactory().hystrixConnectionFactory();
+	}
 }

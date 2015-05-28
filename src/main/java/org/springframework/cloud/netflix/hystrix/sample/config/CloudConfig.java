d33bcdf858e8a848c22939493b5149e606ede19d
Scott Frederick <scottyfred@gmail.com>
2015-05-28 15:52:59 -0500

    Updated spring cloud connectors dependencies.

--- src/main/java/org/springframework/cloud/netflix/hystrix/sample/config/CloudConfig.java
+++ src/main/java/org/springframework/cloud/netflix/hystrix/sample/config/CloudConfig.java
@@ -1,8 +1,8 @@
 package org.springframework.cloud.netflix.hystrix.sample.config;
 
+import io.pivotal.spring.cloud.config.java.CloudConnectorsConfig;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.cloud.netflix.hystrix.amqp.HystrixConnectionFactory;
-import org.springframework.cloud.pivotal.config.java.CloudConnectorsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
